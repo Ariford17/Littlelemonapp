992703f9a4f7220f7d707f53d1f8a3edc920acf0
Ariford17 <seinmyo.smh178@gmail.com>
2023-10-24 02:40:19 +0630

    Updating new

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+export { default as Navbar }
+from './Navbar/Navbar';
+export { default as Header }
+from './Header/Header';
+export { default as Main }
+from './Main/Main';
+export { default as Footer }
+from './Footer/footer';
+export { default as Reservations }
+from './Reservations/Reservations';
+export { default as Order }
+from './Order/Order';
